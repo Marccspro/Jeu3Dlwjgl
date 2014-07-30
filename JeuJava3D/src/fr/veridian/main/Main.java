d1cbc37099e83905dbfa72083eb2f28893466191
Marc <Marc@Marc-PC>
2014-07-30 12:23:42 +0200

    épisode 5

--- JeuJava3D/src/fr/veridian/main/Main.java
+++ JeuJava3D/src/fr/veridian/main/Main.java
@@ -1,13 +1,22 @@
 package fr.veridian.main;
 
+import org.lwjgl.util.vector.Vector3f;
+
+import fr.veridian.main.render.Camera;
 import fr.veridian.main.render.DisplayManager;
+
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
 	
 	boolean running = false;
 	
+	Camera cam;
+	
 	public Main() {
 		DisplayManager.create(720, 480, "Jeu 3D lwjgl");
+		cam = new Camera(new Vector3f(0, 0, 0));
+		cam.setPerspectiveProjection(70.0f, 0.1f, 100.0f);
 	}
 	
 	public void start() {
@@ -28,8 +37,27 @@ public class Main {
 		while(running) {
 			if (DisplayManager.isClosed()) stop();
 			DisplayManager.update();
+			render();
 		}
 		exit();
+	}
+	
+	public void render() {
+		cam.getPerspectiveProjection();
+		
+		glBegin(GL_QUADS);
+			glColor3f(0.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, -0.5f, -1.0f);
+			
+			glColor3f(1.0f, 0.0f, 1.0f);
+			glVertex3f(1.0f, -0.5f, -1.0f);
+			
+			glColor3f(1.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, -0.5f, -3.0f);
+			
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, -0.5f, -3.0f);
+		glEnd();
 	}
 	
 	public static void main(String[] args) {
